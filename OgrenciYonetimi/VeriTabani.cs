cb6469b30f206890462c54b783666dde8faf23d2
onurperci <onur_perci@hotmail.com>
2021-12-26 18:36:40 +0300

    Proje dosyası ekle.

---
+++ OgrenciYonetimi/VeriTabani.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace OgrenciYonetimi
+{
+    class VeriTabani
+    {
+        public static int sayac = 0;
+        public static object[,] ogrenci = new object[sayac,5];
+        public static ArrayList[,] deneme = new ArrayList[0,0];
+
+    }
+}
---
+++ OgrenciYonetimi/kayit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciYonetimi
+{
+    class kayit
+    {
+      
+        public static void kayitİslemi()
+        {
+            Console.Write("Kaç öğrenci kaydetmek istiyorsunuz :");
+            int oKayitSayisi=int.Parse(Console.ReadLine());
+
+            VeriTabani.sayac += oKayitSayisi;
+
+            for (int i = 0; i < oKayitSayisi; i++)
+            {
+                Conso
+            }
+
+
+        }
+    }
+}
